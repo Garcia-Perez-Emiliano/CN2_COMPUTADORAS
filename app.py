e1be1f34f88936e68e4eb3d09f5c741b5373e4b9
Eduardo <l22590417@sjuanrio.tecnm.mx>
2026-09-24 17:35:27 -0600

    Implementar la eliminación

--- app.py
+++ app.py
@@ -38,5 +38,17 @@ def index():
         computadoras=computadoras
     )
 
+@app.route('/eliminar/<int:id>', methods=['POST'])
+def eliminar(id):
+    # Buscar el registro por su ID
+    computadora = InventarioComputadora.query.get_or_404(id)
+    
+    # Eliminar el registro de la base de datos
+    db.session.delete(computadora)
+    db.session.commit()
+    
+    # Redirigir a la función index que carga el inventario
+    return redirect(url_for('index'))
+
 if __name__ == '__main__':
     app.run(debug=True)
